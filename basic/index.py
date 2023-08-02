3a6caa74b30fe2cdde15b59c7ea00d0a0680b29e
abdul saeed <abdulsaeed1356@gmail.com>
2023-08-02 16:56:33 +0500

    work done

--- basic/index.py
+++ basic/index.py
@@ -1,2 +1,12 @@
-print ( "hello")
-print ( 4)
+# var1 is in the global namespace
+var1 = 5
+def some_func():
+  print ("hey")
+	# var2 is in the local namespace
+	var2 = 6
+	def some_inner_func():
+
+		# var3 is in the nested local
+		# namespace
+		var3 = 7
+some_func ()
